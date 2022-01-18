0f2a59855ec2eb110823ff990d0dcbcd668a7616
Szymon Prusak <szymon.prusak.1@gmail.com>
2022-01-18 20:36:44 +0100

    visualisation

--- division/regiongrouper.py
+++ division/regiongrouper.py
@@ -1,4 +1,6 @@
 import sys
+from itertools import cycle
+from matplotlib import pyplot as plt
 
 from geometry import Point2D, Rectangle2D
 from gparser import GraphParser
@@ -11,6 +13,15 @@ class RegionCenter:
     def __init__(self, region_id: int, location: Point2D):
         self.region_id = region_id
         self.location = location
+
+def plot_points(pointsCollection, color):
+    x_coordinates = []
+    y_coordinates = []
+
+    for point in pointsCollection:
+        x_coordinates.append(point.x)
+        y_coordinates.append(point.y)
+    plt.plot(x_coordinates, y_coordinates, 'o', color=color)
 
 
 region_filename = sys.argv[1]
@@ -43,9 +54,15 @@ for r in region_centers:
     tree.add(r)
 tree.divide()
 
+colors = cycle("bgrcmyk")
+
 region_regions = tree.get_regions()
 with open(region_assignment_filename, 'x') as region_assignment_filestream:
     for r in region_regions:
         region_data = [str(rc.region_id) for rc in r.items]
         region_data_str = str.join(" ", region_data)
         region_assignment_filestream.write(region_data_str + '\n')
+
+        plot_points([v.location for v in r.items], next(colors))
+
+plt.show()
